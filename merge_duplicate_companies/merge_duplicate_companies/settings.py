faa463267ebf73737a817e387955d2e8aa162d38
Oleg <slepcov_oleg@mail.ru>
2023-06-22 23:45:59 +0700

    Fixed bag

--- merge_duplicate_companies/merge_duplicate_companies/settings.py
+++ merge_duplicate_companies/merge_duplicate_companies/settings.py
@@ -44,11 +44,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.duplicate_company_resolver.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.duplicate_company_resolver.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
