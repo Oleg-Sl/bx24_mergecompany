3f515a6f9fdf76fa1c15387b4e42f0290e9cee3b
Oleg <slepcov_oleg@mail.ru>
2023-06-23 11:20:18 +0700

    Added filter requests by ID company

--- merge_duplicate_companies/merge_duplicate_companies/middleware.py
+++ merge_duplicate_companies/merge_duplicate_companies/middleware.py
@@ -6,14 +6,14 @@ class RequestFilterMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # id_company = request.data.get("data[FIELDS][ID]", None)
-        if 'data[FIELDS][ID]' in request.data:
-            id_company = request.data.get('data[FIELDS][ID]')
-            cache_key = f'request_{id_company}'
+        if request.method == 'POST':
+            if 'data[FIELDS][ID]' in request.POST:
+                id_company = request.POST.get('data[FIELDS][ID]')
+                cache_key = f'request_{id_company}'
 
-            if cache.get(cache_key):
-                return HttpResponseForbidden('Duplicate request')
+                if cache.get(cache_key):
+                    return HttpResponseForbidden('Duplicate request')
 
-            cache.set(cache_key, True, timeout=60)  # Хранить данные в кэше на 60 секунд
+                cache.set(cache_key, True, timeout=60)  # Хранить данные в кэше на 60 секунд
 
         return self.get_response(request)
